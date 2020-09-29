3bbea7100ba8ade87b9e6dedd8c897353dd756e0
Natascha <NataschaHof@gmx.de>
2020-09-29 20:01:05 +0200

    branch-fix

--- src/routes/trainsRoutes.js
+++ src/routes/trainsRoutes.js
@@ -6,6 +6,10 @@ const trainsController = require("../controllers/trainsController");
 // get multiple trains by StopId
 router.get("/:stationId", trainsController.getByStationId);
 
+// get all trains
 router.get("/", trainsController.getAllTrains);
+
+// update maintenance property with trainid
+router.put("/:id", trainsController.setMaintenance);
 
 module.exports = router;
